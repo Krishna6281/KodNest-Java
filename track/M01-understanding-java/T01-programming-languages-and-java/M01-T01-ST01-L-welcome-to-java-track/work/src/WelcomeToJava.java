3a615c9a3ae98d6affa0a812716e02a89e18a3f3
krishna6281 <ambavaramkr6964@gamil.com>
2026-07-30 12:49:15 +0530

    day1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,12 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("Welcome to the Java Track");
+        System.out.println("I already know to build programming logic. ");
+        System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("My name is Krishna.");
+        System.out.println("My goal is to become a Java Developer.");
+        System.out.println("I selected to Java because I want to build large applications.");
     }
 }
