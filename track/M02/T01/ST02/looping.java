817cdfdccfa345e8de419280834a6dce50a6f037
krishna6281 <ambavaramkr6964@gamil.com>
2026-08-04 13:01:46 +0530

    day2

--- track/M02/T01/ST02/looping.java
+++ track/M02/T01/ST02/looping.java
@@ -2,7 +2,7 @@
 class looping {
 
     public static void main(String[] args) {
-        int num = 25;
+        int num = -5;
         if (num > 0) {
             System.out.println("positive");
         } else if (num < 0) {
