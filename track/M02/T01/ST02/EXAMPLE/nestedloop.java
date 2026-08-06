c91d3f55bbf7a89963fabdc28a07e6ffbae6577c
krishna6281 <ambavaramkr6964@gamil.com>
2026-08-06 12:30:15 +0530

    123

--- track/M02/T01/ST02/EXAMPLE/nestedloop.java
+++ track/M02/T01/ST02/EXAMPLE/nestedloop.java
@@ -1,27 +1,6 @@
 
 class nestedloop {
 
-    // public static void main(String[] args) {
-    //     int i = 1;
-    //     while (i <= 5) {
-    //         int j = 1;
-    //         while (j <= 5) {
-    //             System.out.println(j);
-    //             j++;
-    //         }
-    //         System.out.println();
-    //         i++;
-    //     }
-    // }
-// public static void main (String [] args)
-// {
-//     for(int i=1;i<=5;i++){ 
-//         for(int j=1;j<=5;j++){
-//             System.out.println(j);
-//         }
-//         System.out.println();
-//     }
-// }}
     public static void main(String[] args) {
         int i = 1;
         do {
