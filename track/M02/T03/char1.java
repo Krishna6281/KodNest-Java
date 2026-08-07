030fc71ee54b7d8d7b389e00acbcd5180b20605b
krishna6281 <ambavaramkr6964@gamil.com>
2026-08-07 12:21:41 +0530

    user input

---
+++ track/M02/T03/char1.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+
+class char1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(5);
+        System.out.println(c);
+    }
+}
